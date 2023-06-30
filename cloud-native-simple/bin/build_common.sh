692e0ae2785f8887089d9de64f50764cd9d9d13d
Marc Gueury <marc.gueury@oracle.com>
2023-06-30 16:25:08 +0200

    2023-06-30

--- cloud-native-simple/bin/build_common.sh
+++ cloud-native-simple/bin/build_common.sh
@@ -1,6 +1,6 @@
 # Build_common.sh
 BIN_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-APP_DIR=`echo ${SCRIPT_DIR} |sed -E "s/(.*)\/(.*)\//\2/g"`
+APP_DIR=`echo ${SCRIPT_DIR} |sed -E "s/(.*)\/(.*)/\2/g"`
 
 # SCRIPT_DIR should be set by the calling scripts 
 cd $SCRIPT_DIR
