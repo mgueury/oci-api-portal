fadded0798e5f6009003c7fe5e0ebbc6e0c35bd8
Marc Gueury <marc.gueury@oracle.com>
2025-06-08 15:49:46 +0200

    yaml lib not there anymore

--- other/cloud-native-sample/group_common/env.sh
+++ other/cloud-native-sample/group_common/env.sh
@@ -20,17 +20,18 @@ else
   # Ex: export TF_VAR_compartment_ocid=ocid1.compartment.xxxxx
   export TF_VAR_compartment_ocid=__TO_FILL__
   # Ex: export APIM_HOST=xxxx-xxx.adb.region.oraclecloudapps.com
-  export APIM_HOST=__TO_FILL__
   
   # TF_VAR_license_model : BRING_YOUR_OWN_LICENSE or LICENSE_INCLUDED
   export TF_VAR_license_model="LICENSE_INCLUDED"
 
-  # Landing Zone
-  # export TF_VAR_lz_appdev_cmp_ocid=$TF_VAR_compartment_ocid
-  # export TF_VAR_lz_database_cmp_ocid=$TF_VAR_compartment_ocid
-  # export TF_VAR_lz_network_cmp_ocid=$TF_VAR_compartment_ocid
-  # export TF_VAR_lz_security_cmp_ocid=$TF_VAR_compartment_ocid
 fi
+export APIM_HOST=__TO_FILL__
+
+# Landing Zone
+# export TF_VAR_lz_appdev_cmp_ocid=$TF_VAR_compartment_ocid
+# export TF_VAR_lz_database_cmp_ocid=$TF_VAR_compartment_ocid
+# export TF_VAR_lz_network_cmp_ocid=$TF_VAR_compartment_ocid
+# export TF_VAR_lz_security_cmp_ocid=$TF_VAR_compartment_ocid
 
 # Get other env variables automatically (-silent flag can be passed)
 . $BIN_DIR/auto_env.sh $1
